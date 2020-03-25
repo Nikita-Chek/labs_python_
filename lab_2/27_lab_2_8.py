699ce262580d703549b620e18f1c26684b4e54aa
Nikita-Chek <61079589+Nikita-Chek@users.noreply.github.com>
2020-03-25 17:05:38 +0300

    Putin

--- lab_2/27_lab_2_8.py
+++ lab_2/27_lab_2_8.py
@@ -1,22 +1,6 @@
 import argparse
 import math
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-n', '--number', type=float)
-parser.add_argument('-c','--chek', type=bool)
-n = parser.parse_args().number
-if n < 0:
-	print("input non negative number")
-
-while n < 0:
-	print("non negative number")
-	n = int(input())
-
-flag = False
-flag = parser.parse_args().chek
-
-
-
 def is_power_of2(n):
 	if n == 0:
 		return False
@@ -29,6 +13,23 @@ def is_power_of2_chek(n):
 		n = int(n)
 		return n & (n-1) == 0
 	return False
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-n', '--number', type=int, help='input non negative number')
+parser.add_argument('-c','--chek', type=bool, help='True/False if you need to chek')
+inputs = parser.parse_args()
+
+n = inputs.number
+
+
+if n is None or n < 0:
+	print("input non negative number")
+	n = -1
+	while n < 0:
+		n = int(input()) 
+
 print(is_power_of2(n))
+
+flag = inputs.chek
 if flag:
 	print(is_power_of2_chek(n))
