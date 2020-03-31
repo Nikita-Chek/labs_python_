ce1642b9414e798543e5ffd94efbc07f75579b2d
Nikita-Chek <61079589+Nikita-Chek@users.noreply.github.com>
2020-03-31 20:41:28 +0300

    Rasputin

--- lab_2/27_lab_2_1.py
+++ lab_2/27_lab_2_1.py
@@ -4,29 +4,6 @@ import sys
 import argparse
 import math
 
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument('-a', '--args', nargs='+', type=int, help='input array')
-parser.add_argument('-s', type=int, help='start point')
-parser.add_argument('-e', type=int, help='end point')
-parser.add_argument('-c','--chek', type=bool, help='if you need to chek')
-parser.add_argument('-f', '--file', type=bool, help='if you need to input from file')
-
-inputs = parser.parse_args()
-array = inputs.args
-start = inputs.s
-end = inputs.e
-
-flag1 = inputs.chek
-flag2 = inputs.file
-
-if flag2:
-	file = open('additional/for_first.txt','r')
-	array = file.read().split(' ')
-	array = [int(_) for _ in array]
-	file.close()
-
 def sum_sqrt(array, start, end):
 	sum = 0
 	length = len(array)
@@ -51,6 +28,27 @@ def sum_chek(array, start, end):
 		sum += array[x]
 	return sum
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-a', '--args', nargs='+', type=int, help='input array')
+parser.add_argument('-s', type=int, help='start point')
+parser.add_argument('-e', type=int, help='end point')
+parser.add_argument('-c','--chek', type=bool, help='if you need to chek')
+parser.add_argument('-f', '--file', type=bool, help='if you need to input from file')
+
+inputs = parser.parse_args()
+array = inputs.args
+start = inputs.s
+end = inputs.e
+
+flag1 = inputs.chek
+flag2 = inputs.file
+
+if flag2:
+	file = open('additional/for_first.txt','r')
+	array = file.read().split(' ')
+	array = [int(_) for _ in array]
+	file.close()
 
 print(sum_sqrt(array, start, end))
 if flag1:
