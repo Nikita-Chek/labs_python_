3ff4b5be97c828b70bc160c365acc547875e6b5d
Nikita-Chek <61079589+Nikita-Chek@users.noreply.github.com>
2020-04-24 19:58:43 +0300

    Pushkin

--- lab_2/27_lab_2_4.py
+++ lab_2/27_lab_2_4.py
@@ -2,16 +2,6 @@ import argparse
 import json
 import os
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--file', type=str)
-file = parser.parse_args().file
-
-with open('additional/array_json.json', 'r') as f:
-	data = json.load(f)
-
-List = data[""]
-#List = [[2,3], [3, 3, 'er'], [[4], [3,[[6,[3],5,2]]]], [[[6]]]]
-
 def flatten(List, fl = []):
 	for _ in List:
 		if type(List[_]) is list:
@@ -26,7 +16,19 @@ def gen_flatten(List):
 			yield from gen_flatten(_)
 		else:
 			yield _
-l = []
-for _ in gen_flatten(List):
-	l.append(_)
-print(l)
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-f', '--file', type=str)
+	file = parser.parse_args().file
+	with open('additional/array_json.json', 'r') as f:
+		data = json.load(f)
+
+	List = data[""]
+	#List = [[2,3], [3, 3, 'er'], [[4], [3,[[6,[3],5,2]]]], [[[6]]]]
+
+	l = []
+	for _ in gen_flatten(List):
+		l.append(_)
+	print(l)
